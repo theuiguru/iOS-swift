2c382c6c169d090a0dca72c9b678c27cdd6faeec
Chris Thomas <webmaster102@gmail.com>
2018-12-12 23:34:00 -0500

    added tippy

---
+++ tippy/tippyTests/tippyTests.swift
@@ -0,0 +1,34 @@
+//
+//  tippyTests.swift
+//  tippyTests
+//
+//  Created by Chris Thomas on 12/11/18.
+//  Copyright © 2018 Chris Thomas. All rights reserved.
+//
+
+import XCTest
+@testable import tippy
+
+class tippyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
